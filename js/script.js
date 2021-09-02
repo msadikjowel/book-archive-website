cc37be6172041c427a470d6428c54f8bc8196947
Mohammed Sadik <msadikjowel@gmail.com>
2021-09-02 13:36:56 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -5,32 +5,35 @@ const bookContainer = document.getElementById('book-container')
 const result = document.getElementById('result')
 const errorMessage = document.getElementById('error')
 
+// catch input data when button clicked
 const search = () => {
 
+    // info message for user wait when button clicked
     const inputText = inputField.value;
     if (inputText !== 0) {
         errorMessage.innerHTML = `You searched for <span class = "fw-bold"> ${inputText} </span>, please wait for load...`
     }
+
+    // fetching data from API
     fetch(`https://openlibrary.org/search.json?q=${inputText}`)
         .then(res => res.json())
         .then(data => displayBook(data.docs))
 
-    // clear input data
+    // clear input data when search button clicked
     inputField.value = '';
-    // clear data when again click
+    // clear body data when click on search button
     bookContainer.textContent = '';
     result.textContent = '';
 };
 
-
+// showing data after search button clicked
 const displayBook = books => {
 
     // checking error message
     if (books.length === 0) {
         errorMessage.innerText = 'No result found'
-        resultPara.innerHTML = ''
+        return errorMessage;
     }
-
 
     // slicing books
     const bookSlice = books.slice(0, 25)
@@ -38,16 +41,16 @@ const displayBook = books => {
     // showing how many results found
     const resultPara = document.createElement('p');
     resultPara.innerHTML = `
-        <p class = "fw-light"> ${bookSlice.length} results showing from ${books.length} results <p>
+        <p class = "fw-light"> ${bookSlice.length} results found <p>
     `
     result.appendChild(resultPara);
 
     // delete info message when data loaded
     errorMessage.innerHTML = ''
 
-    // 
+    // adding loaded book details with forEach in body
     bookSlice.forEach(book => {
-        console.log(book)
+        // console.log(book)
         const bookDiv = document.createElement('div')
         bookDiv.classList.add('col')
 
@@ -56,6 +59,7 @@ const displayBook = books => {
             <img
             style = "width: 200px; height:300px"
              class = "img-fluid rounded mx-auto d-block" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+             
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">By: ${book.author_name}</p>
